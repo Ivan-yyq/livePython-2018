fe0f7223740345a736dbb6f7e4169b8d8b9d0862
tony <1478458905@qq.com>
2018-05-11 15:58:55 +0800

    2018-5-8 day16

---
+++ day16/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/8 21:50
+# @Author  : yangyuanqiang
+# @File    : __init__.py.py
---
+++ day16/demon3.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/8 21:42
+# @Author  : lingxiangxiang
+# @File    : demon3.py
+
+'''
+
+
+select 列名 from 表名 where 条件判断
+
+select * from sutdent where group by stdname
+
+               a, c where a.id = c.组id
+
+select a.id a.name, c.id from a join c on c.组id = a.id and ^^^^
+select * from table_name limiet 10;
+#查询用户
+select user,host from mysql.user;
+
+
+#查询表的结构
+show create table_name
+desc table_name
+
+
+#插入数据
+insert into table_name (id, name, age) values(1, 'ling', 18), (), (), ();
+
+#删除数据
+delete from table where 条件判断
+
+truncate  只清楚数据，不删除表结构
+drop    表结果都给你干掉了
+
+#更新数据
+update table_name set 列名=xxx, where 条件
+
+create index 库名_表名_列名1_列名2 （列名1， 列名2）
+
+
+'''
