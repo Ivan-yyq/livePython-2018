341cfc860cff9941ca135265310b20f849def54c
tony <1478458905@qq.com>
2018-05-11 15:49:47 +0800

    2018-3-26 day1

---
+++ day1/hello_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/28 23:31
+# @Author  : lingxiangxiang
+# @File    : hello_name.py
+
+name = input("print input your name: ")
+print("hello " + name)
+
+
+
+
+
---
+++ day1/helloworld.py
@@ -0,0 +1,5 @@
+# _*_coding:utf-8_*_
+# Author: yangyuanqiang
+# date: 2018-03-07
+
+print('Hello world!')
