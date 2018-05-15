21e80ec8f02a7fadb6045305e9638a0c2d13033c
tony <1478458905@qq.com>
2018-05-15 17:48:59 +0800

    day1

--- day1/demon2.py
+++ day1/demon2.py
@@ -13,20 +13,10 @@ print("程序名字是： {0}".format(sys.argv[0]))
 print("参数1 = {0}".format(sys.argv[1]))
 print("参数2 = {0}".format(sys.argv[2]))
 print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
+
 
 # for i in range(0, 100):
 #     test(i)
 
 
-
-print("demon2")
-print("demon2")
-print("demon2")
-print("demon2")
+# print("demon2")
