96990658118c035c628afee6dd374eee64021b1b
ivan <15625087150@163.com>
2018-05-27 09:36:50 +0800

    2018-5-26 day24

---
+++ day24/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/26 22:05
+# @Author  : yangyuanqiang
+# @File    : __init__.py.py
---
+++ day24/test/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/27 09:12
+# @Author  : yangyuanqiang
+# @File    : __init__.py.py
