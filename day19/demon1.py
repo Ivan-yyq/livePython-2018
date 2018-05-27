3425e335db2197b5989fcdc9bfd220179c6bbe05
ivan <15625087150@163.com>
2018-05-27 10:04:11 +0800

    day19

--- day19/demon1.py
+++ day19/demon1.py
@@ -9,13 +9,13 @@
 import redis
 
 #创建redis链接对象
-r = redis.Redis(host='192.168.3.20',port=6379,decode_responses=True)
+r = redis.Redis(host='192.168.3.11',port=6379,decode_responses=True)
 #存储键值对
 r.set('site','www.qi.cn')
 #获取值
 print(r.get('site'))
 #指定decode_responses为True，表示输出为字符串
-red = redis.StrictRedis(host='192.168.3.20',port=6379,decode_responses=True)
+red = redis.StrictRedis(host='192.168.3.11',port=6379,decode_responses=True)
 
 #默认redis入库编码是utf-8，如果要修改的话，需要指明 charset 和 decode_responsers 为True
 #test = redis.StrictRedis(host='localhost', port=6379, db=0, password=None, socket_timeout=None, connection_pool=None, charset='utf-8', errors='strict', decode_responses=False, unix_socket_path=None)
