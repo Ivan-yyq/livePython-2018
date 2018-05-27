82e176b86e8cd67c9c9eff80ea04c054b70870de
ivan <15625087150@163.com>
2018-05-27 10:03:28 +0800

    2018-5-24 day23

--- day23/demon6.py
+++ day23/demon6.py
@@ -11,5 +11,6 @@ re1 = taskA.delay(100, 200)
 print(re1.result)
 re2 = taskB.delay(1, 2, 3)
 print(re2.result)
-re3 = add.delay(1, 2, 3)
+re3 = add.delay(1, 2)
 print(re3.status)     #PENDING
+print(re3.result)
--- day23/tasks.py
+++ day23/tasks.py
@@ -20,12 +20,9 @@ def taskB(x,y,z):
     return x + y + z
 
 
-
-
-
 @app.task
-def add(x,y,z):
-    return x + y + z
+def add(x,y):
+    return x + y
 
 
 
