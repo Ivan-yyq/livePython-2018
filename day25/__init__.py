e8959f8dd2171cc5dc41ee606766c9250ccac473
ivan <15625087150@163.com>
2018-05-29 22:14:53 +0800

    2018-5-29 day25

---
+++ day25/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/29 20:46
+# @Author  : yangyuanqiang
+# @File    : __init__.py.py
---
+++ day25/createtable.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/29 22:11
+# @Author  : yangyuanqiang
+# @File    : createtable.py
+
+
+import codecs
+
+
+
+from sqlalchemy import Column, MetaData, Table
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('mysql+pymysql://xiang:xiang@192.168.48.136/sqlalchemy')
+
+metadata = MetaData(engine)
+
+dictionary = Table('dictionary', metadata,
+             Column('id', Integer, primary_key=True),
+             Column('key', String(50)),
+             Column('value', String(50))
+             )
+metadata.create_all(engine)
