fdf4eadc54ba6f7a1ccf5a5a2a97bb75581ce0fb
ivan <15625087150@163.com>
2018-06-05 20:51:15 +0800

    2018-6-3 day27

---
+++ day27/demon1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/6/5 20:40
+# @Author  : yangyuanqiang
+# @File    : demon1.py
+
+
+import socketserver
+import http.server
+
+port = "8888"
+host = "192.168.3.11"
