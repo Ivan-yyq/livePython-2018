6d8667acd8b6c44c7ea75c20000ada459b811d1a
ivan <15625087150@163.com>
2018-06-05 22:39:54 +0800

    2018-6-3 day27

--- day27/demon1.py
+++ day27/demon1.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 2018/6/5 20:40
-# @Author  : yangyuanqiang
+# @Time    : 2018/6/3 20:22
+# @Author  : lingxiangxiang
 # @File    : demon1.py
 
 
-import socketserver
-import http.server
+import socket
 
-port = "8888"
-host = "192.168.3.11"
+host = "www.baidu.com"
+port = 443
+
+ip = socket.gethostbyname(host)
+print(ip)
+
+address = (ip, port)
+
+try:
+
+    s = socket.socket()
+
+    s.connect(address)
+    request = "GET / HTTP/1.1\r\nHost:www.baidu.com\r\n\r\n"
+    s.sendall(request.encode("utf-8"))
+    res = s.recv(80960)
+    if res:
+        print(res)
+    else:
+        print("visit {0} error".format(host))
+except Exception as e:
+    raise e
